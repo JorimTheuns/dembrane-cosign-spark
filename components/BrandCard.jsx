06ad8da8767082afc5d0dcb123468e4cae33b620
Jorim Theuns <jorim.theuns@gmail.com>
2022-08-10 14:06:36 +0200

    mobile support

--- components/BrandCard.jsx
+++ components/BrandCard.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { useState } from "react";
+import VisibilitySensor from "react-visibility-sensor";
+import { isMobile } from "react-device-detect";
 
 export default function BrandCard({
   children,
@@ -25,27 +27,42 @@ export default function BrandCard({
         >
           {title}
         </h1>
-        <div
-          onMouseEnter={() => setActiveCard(true)}
-          onMouseLeave={() => setActiveCard(false)}
-          className=""
+        <VisibilitySensor
+          onChange={(isVisible) => {
+            if (isMobile) {
+              this.setState({ setActiveCard: isVisible });
+            }
+          }}
         >
-          <div className="active:scale-90 hover:scale-95 transition-all duration-150 ease-in-out cursor-pointer relative">
-            <div className="">
-              <img
-                src={bawLink}
-                className="transition-all duration-500 ease-in-out opacity-100 hover:opacity-0"
-              />
-            </div>
-            <div className=" top-0 left-0 absolute">
-              <img
-                src={colorLink}
-                className="transition-all duration-500 ease-in-out opacity-0 hover:opacity-100"
-              />
+          <div
+            onMouseEnter={() => {
+              if (!isMobile) {
+                setActiveCard(true);
+              }
+            }}
+            onMouseLeave={() => {
+              if (!isMobile) {
+                setActiveCard(false);
+              }
+            }}
+            className=""
+          >
+            <div className="active:scale-90 hover:scale-95 transition-all duration-150 ease-in-out cursor-pointer relative">
+              <div className="">
+                <img
+                  src={bawLink}
+                  className="transition-all duration-500 ease-in-out opacity-100 hover:opacity-0"
+                />
+              </div>
+              <div className=" top-0 left-0 absolute">
+                <img
+                  src={colorLink}
+                  className="transition-all duration-500 ease-in-out opacity-0 hover:opacity-100"
+                />
+              </div>
             </div>
           </div>
-        </div>
-
+        </VisibilitySensor>
         <div
           className={`max-w-[250px] text-center transition-all duration-1000 ease-in-out" + ${
             activeCard
